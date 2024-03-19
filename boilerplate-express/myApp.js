3cd0a16f005becaca32c72e582b367a967c09d4a
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-19 22:06:11 +0300

    installed dotenv package and using .env file

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 let express = require("express");
 let app = express();
 
@@ -7,18 +9,26 @@ module.exports = app;
 //   res.send("Hello Express");
 // });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/views/index.html");
 
-// Normal usage
-app.use(express.static(__dirname + "/public"));
+// });
 
-// Assets at the /public route
-app.use("/public", express.static(__dirname + "/public"));
+// // Normal usage
+// app.use(express.static(__dirname + "/public"));
+
+// // Assets at the /public route
+// app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-    res.json({ message: "Hello json" });
+  // res.json({ message: "Hello json" });
+  const messageStyle = process.env.MESSAGE_STYLE
+
+  if (messageStyle === "uppercase") {
+    res.json({"message": "HELLO JSON" });
+  } else {
+    res.json({ "message": "Hello json" });
+  }
 });
 
-// app.listen(8080);
+// // app.listen(8080);
