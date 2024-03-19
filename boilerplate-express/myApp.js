5e3f84aa5b17994697ff0f921dc3b04d9d4dc388
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-19 10:17:31 +0300

    sending assets

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -7,6 +7,16 @@ module.exports = app;
 //   res.send("Hello Express");
 // });
 
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
+
+// Normal usage
+// app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
+
+app.listen(8080);
