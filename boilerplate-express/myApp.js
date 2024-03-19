ef8a3c2be9dc40e94730e6c21157ea4038b21a52
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-19 09:50:02 +0300

    sending a file on a root path

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -3,6 +3,10 @@ let app = express();
 
 module.exports = app;
 
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
+
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 });
