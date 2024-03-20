cb4d1a6f4fcc096af7dd8eda4e5491829338d60d
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-20 09:49:43 +0300

    Chain Middleware

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -31,11 +31,20 @@ module.exports = app;
 //   }
 // });
 
+// app.use((req, res, next) => {
+//   console.log(`${req.method} / ${req.path} - ${req.ip}`);
+//   next();
+// })
 
-app.use((req, res, next) => {
-  console.log(`${req.method} / ${req.path} - ${req.ip}`);
-  next();
-})
-
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
 // // app.listen(8080);
