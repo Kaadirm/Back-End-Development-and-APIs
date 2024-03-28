be5e5b7a95356e6e244be52a85d8517a86d39436
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-29 00:28:43 +0300

    connected to database

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -1,5 +1,17 @@
-require('dotenv').config();
+const mongoose = require("mongoose");
 
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
 
 let Person;
 
