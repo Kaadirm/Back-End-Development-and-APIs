8448be4a5d50bb20be63405cbecaacc10aa60f79
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-30 13:47:22 +0300

    Use model.findById() to Search Your Database By _id

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -67,7 +67,12 @@ const findOneByFood = (food, done) => {
 
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (error, data) => {
+    if (error) {
+      return console.error(error);
+    }
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
