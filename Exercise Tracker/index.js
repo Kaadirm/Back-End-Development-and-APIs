e61cda47d84078e49c4e9a2654986342868e170f
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-18 23:37:56 +0300

    connected to database

--- Exercise Tracker/index.js
+++ Exercise Tracker/index.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
-
+mongoose
+    .connect(process.env.MONGO_URI, {})
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((error) => {
+        console.log('Failed to connect to the database:', error);
+    });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+    console.log('Your app is listening on port ' + listener.address().port);
+});
